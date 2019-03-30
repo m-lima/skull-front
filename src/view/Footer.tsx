f773c75a96d8321b6412969056a516c96b3f0ab0
Marcelo Lima <marcelo@telenordigital.com>
2019-03-30 16:27:18 +0100

    Consuming UI ready

--- src/view/Footer.tsx
+++ src/view/Footer.tsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import './css/Footer.css'
+import { Icon } from './Icon'
+import * as Config from '../model/Config'
 
 export default class Footer extends Component {
   render() {
     return (
-      <div className='Footer' >
-        <i className='fas fa-file-download' />
+      <div className='Footer'>
+        <div className='Footer-container'>
+          <a href={Config.Endpoint.skull} title='Download JSON'>
+            <Icon icon='fas fa-file-download' onClick={() => console.log('YOO!!')}/>
+          </a>
+          <a href='/chart' title='Go to chart'>
+            <Icon icon='fas fa-chart-line' />
+          </a>
+        </div>
       </div>
     )
   }
--- src/view/SkullButton.tsx
+++ src/view/SkullButton.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './css/SkullButton.css'
 import IQuickValue from '../model/IQuickValue'
+import { Icon } from './Icon'
 
 export default class SkullButton extends Component<IQuickValue> {
   render() {
     return (
-      <div className='Skull-button'>
-        <i className={this.props.icon} />
+      <div className='Skull-button' title={this.props.type + ': ' + this.props.amount}>
+        <Icon icon={this.props.icon} />
       </div>
     )
   }
