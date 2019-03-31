bd52702c77b6289e16915e429aac75f69ae8168b
Marcelo Lima <marcelo@telenordigital.com>
2019-03-31 12:44:55 +0200

    Allow reload on failure

--- src/view/Message.tsx
+++ src/view/Message.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import './css/Message.css'
 
+import * as Config from '../model/Config'
+import Access from '../control/Access'
+
 export const Loading = () =>
   <div className='Message'>
     <i className='fas fa-spinner' id='Message-loading-spinner' />
@@ -11,10 +14,16 @@ export const Error = () =>
   <div className='Message'>
     <i className='fas fa-sad-tear' />
     Something went wrong..
+    <div className='Message-action' onClick={() => window.location.reload()}>
+      Refresh
+    </div>
   </div>
 
 export const Unauthorized = () =>
   <div className='Message'>
     <i className='fas fa-fingerprint' />
     Unauthorized
+    <div className='Message-action' onClick={Access.logout}>
+      Logout
+    </div>
   </div>
